34af4d76e65ae3e647f16102c4aa38885a684c54
daisu <daisu@DESKTOP-CPQM8BT>
2023-05-05 12:00:00 +0900

    課題修正

--- src/main/java/com/techacademy/controller/EmployeeController.java
+++ src/main/java/com/techacademy/controller/EmployeeController.java
@@ -1,26 +1,21 @@
 package com.techacademy.controller;
-
 import java.util.Optional;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import com.techacademy.entity.Employee;
 import com.techacademy.service.EmployeeService;
-
 @Controller
 @RequestMapping("employee")
 public class EmployeeController {
     private final EmployeeService service;
-
     public EmployeeController(EmployeeService service) {
         this.service = service;
     }
-
     /** トップページを表示 */
     @GetMapping({"/", "/list"})
     public String getHome(Model model) {
@@ -29,17 +24,29 @@ public class EmployeeController {
         // home.htmlに画面遷移
         return "/employee/list";
     }
-    
-
+    /** Employee登録画面を表示 */
+    @GetMapping("/create")
+    public String getRegister(@ModelAttribute  Employee employee) {
+        return "employee/create";
+    }
+    /** Employee登録処理 */
+    @PostMapping("/create")
+    public String postRegister(Employee employee) {
+        // User登録
+        service.saveEmployee(employee);
+        // 一覧画面にリダイレクト
+        return "redirect:/employee/list";
+    }
     @GetMapping("/details/{id}")
     public String getEmployeeDetails(@PathVariable("id") Integer id, Model model) {
         Optional<Employee> employee = java.util.Optional.empty();
-		try {
-			employee = EmployeeService.getEmployeeById(id);
-		} catch (Exception e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
-		}
+        try {
+            //employee = EmployeeService.getEmployeeById(id);
+            employee = service.getEmployeeById(id);
+        } catch (Exception e) {
+            // TODO 自動生成された catch ブロック
+            e.printStackTrace();
+        }
         if (employee.isPresent()) {
             model.addAttribute("employee", employee.get());
             return "employee/details";
@@ -47,12 +54,27 @@ public class EmployeeController {
             return "redirect:/employee/list";
         }
     }
- // com.techacademy.controller.EmployeeController
-
     @PostMapping("/delete/{id}")
     public String deleteEmployee(@PathVariable("id") Integer id) {
-        EmployeeService.deleteEmployee(id);
+        service.deleteEmployee(id);
         return "redirect:/employee/list";
     }
+    // ----- 追加:ここから -----
+    /** User更新画面を表示 */
+    @GetMapping("/update/{id}/")
+    public String getEmployee(@PathVariable("id") Integer id, Model model) {
+        // Modelに登録
+        model.addAttribute("user", service.getEmployeeById(id));
+        // User更新画面に遷移
+        return "user/update";
+    }
 
+    /** User更新処理 */
+    @PostMapping("/update/{id}/")
+    public String postUser(Employee employee) {
+        // User登録
+        service.saveEmployee(employee);
+        // 一覧画面にリダイレクト
+        return "redirect:/user/list";
+    }
 }
--- src/main/java/com/techacademy/service/EmployeeService.java
+++ src/main/java/com/techacademy/service/EmployeeService.java
@@ -6,11 +6,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import com.techacademy.entity.Employee;
 import com.techacademy.repository.EmployeeRepository;
@@ -20,58 +15,24 @@ import com.techacademy.repository.EmployeeRepository;
 public class EmployeeService {
 
     @Autowired
-	static
     EmployeeRepository employeeRepository;
 
-    public List<Employee> getEmployeeList() {
+    public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
     }
 
-	public Object getAllEmployees() {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
-	}
+    public Optional<Employee> getEmployeeById(Integer id) {
+        return employeeRepository.findById(id);
+    }
 
-	public static Optional<Employee> getEmployeeById(Integer id) {
-	    return employeeRepository.findById(id);
-	}
-	
-	public static Employee saveEmployee(Employee employee) {
-	    return employeeRepository.save(employee);
-	}
-	
+    public Employee saveEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
 
-	@GetMapping("/create")
-	public String getCreateForm(Model model) {
-	    model.addAttribute("employee", new Employee());
-	    return "employee/create";
-	}
-
-	@PostMapping("/create")
-	public String createEmployee(@ModelAttribute Employee employee) {
-	    EmployeeService.saveEmployee(employee);
-	    return "redirect:/employee/list";
-	}
-
-	@GetMapping("/update/{id}")
-	public String getUpdateForm(@PathVariable("id") Integer id, Model model) {
-	    Optional<Employee> employee = EmployeeService.getEmployeeById(id);
-	    if (employee.isPresent()) {
-	        model.addAttribute("employee", employee.get());
-	        return "employee/update";
-	    } else {
-	        return "redirect:/employee/list";
-	    }
-	}
-
-	@PostMapping("/update")
-	public String updateEmployee(@ModelAttribute Employee employee) {
-	    EmployeeService.saveEmployee(employee);
-	    return "redirect:/employee/list";
-	}
-	public static void deleteEmployee(Integer id) {
-	    employeeRepository.deleteById(id);
-	}
+    public void deleteEmployee(Integer id) {
+        employeeRepository.deleteById(id);
+    }
 }
+
 
 
